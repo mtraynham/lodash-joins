fbf4d87e215aea7262c7d01a99a20139199798cd
Skitch <mrt6467@alum.uncw.edu>
2015-01-29 22:18:57 -0500

    More typescript additions

--- lib/join.ts
+++ lib/join.ts
@@ -15,9 +15,7 @@ export interface IAccessor<O extends Object, A> {
  * @returns {Object}
  * @constructor
  */
-export var DefaultAccessor: IAccessor<Object, Object> = (datum: Object) => {
-    return datum;
-};
+export var DefaultAccessor: IAccessor<Object, Object> = (datum: Object) => datum;
 
 /**
  * IMerger
@@ -35,9 +33,7 @@ export interface IMerger<L extends Object, R extends Object, T extends Object> {
  * @returns {Object}
  * @constructor
  */
-export var DefaultMerger: IMerger<Object, Object, Object> = (left: Object, right: Object) => {
-    return _.extend(new Object(), left, right);
-};
+export var DefaultMerger: IMerger<Object, Object, Object> = (left: Object, right: Object) => _.assign(new Object(), left, right);
 
 /**
  * IJoin
@@ -49,23 +45,90 @@ export var DefaultMerger: IMerger<Object, Object, Object> = (left: Object, right
  */
 export interface IJoin<L extends Object, R extends Object, A, T extends Object> {
     (left: Array<L>, leftAccessor: IAccessor<L, A>,
+     right: Array<R>, rightAccessor: IAccessor<R, A>): Array<T>;
+}
+
+export interface IMergeJoin<L extends Object, R extends Object, A, T extends Object> {
+    (left: Array<L>, leftAccessor: IAccessor<L, A>,
      right: Array<R>, rightAccessor: IAccessor<R, A>,
      merger: IMerger<L, R, T>): Array<T>;
 }
 
-/*
-class Joins {
-    LeftAntiJoin:Join;
-    LeftAntiJoin = <L, R, A, T>(left:Array<L> = [], leftAccessor:Accessor<L, A> = DefaultAccessor,
-                                right:Array<R> = [], rightAccessor:Accessor<R, A> = DefaultAccessor,
-                                merger:Merger<L, R, T> = DefaultMerger) => {
-        var leftValue:A;
-        return _.filter(left, (leftDatum) => {
-            leftValue = leftAccessor(leftDatum);
-            return _.every(right, (rightDatum) => {
-                return _.isEqual(leftValue, rightAccessor(rightDatum));
-            })
-        })
-    }
-}
-*/
+
+export var HashFullOuterJoin: IMergeJoin<Object, Object, any, Object> =
+    (left: Array<Object>, leftAccessor: IAccessor<Object, any>,
+     right: Array<Object>, rightAccessor: IAccessor<Object, any>,
+     merger: IMerger<Object, Object, Object> = DefaultMerger): Array<Object> => {
+        var idx: _.List<Object>,
+            markedValues: any = {},
+            result: Array<Object>,
+            val: any;
+        if (left.length < right.length) {
+            idx = _.groupBy(left, leftAccessor);
+            result = _.reduceRight(right, (previous: Array<Object>, datum: Object) => {
+                markedValues[val = rightAccessor(datum)] = true;
+                if (_.has(idx, val)) {
+                    return _.map(idx[val], (oDatum: Object) => merger(oDatum, datum)).concat(previous);
+                }
+                previous.unshift(datum);
+                return previous;
+            }, []);
+        } else {
+            idx = _.groupBy(right, rightAccessor);
+            result = _.reduceRight(left, (previous: Array<Object>, datum: Object) => {
+                markedValues[val = leftAccessor(datum)] = true;
+                if (_.has(idx, val)) {
+                    return _.map(idx[val], (oDatum: Object) => merger(datum, oDatum)).concat(previous);
+                }
+                previous.unshift(datum);
+                return previous;
+            }, []);
+        }
+        return result.concat(_(idx).filter((value: Object, key: string) => !_.has(markedValues, key)).values().flatten());
+    };
+
+export var HashLeftOuterJoin: IMergeJoin<Object, Object, any, Object> =
+    (left: Array<Object>, leftAccessor: IAccessor<Object, any>,
+     right: Array<Object>, rightAccessor: IAccessor<Object, any>,
+     merger: IMerger<Object, Object, Object> = DefaultMerger): Array<Object> => {
+        var idx: _.Dictionary<Object>,
+            val: any;
+        if (left.length < right.length) {
+            var markedVals: Object = {};
+            idx = _.groupBy(left, leftAccessor);
+            return _.reduceRight(right, (previous: Array<Object>, datum: Object) => {
+                markedVals[val = rightAccessor(datum)] = true;
+                return !_.has(idx, val) ? _.map(idx[val], (oDatum: Object) => merger(oDatum, datum)).concat(previous) : previous;
+            }, []).concat(_(idx).filter((value: Object, key: string) => !_.has(markedValues, key)).values().flatten());
+        } else {
+            idx = _.groupBy(right, rightAccessor);
+            return reduceRight(a, (previous: Array<Object>, datum: Object) => {
+                if (_.has(idx, (val = leftAccessor(datum)))) {
+                    return _.map(idx[val], (oDatum: Object) => merger(datum, oDatum)).concat(previous);
+                }
+                previous.unshift(datum);
+                return previous;
+            }, []);
+        }
+    };
+
+export var HashInnerJoin: IMergeJoin<Object, Object, any, Object> =
+    (left: Array<Object>, leftAccessor: IAccessor<Object, any>,
+     right: Array<Object>, rightAccessor: IAccessor<Object, any>,
+     merger: IMerger<Object, Object, Object> = DefaultMerger): Array<Object> => {
+
+    };
+
+export var HashLeftAntiJoin: IJoin<Object, Object, any, Object> =
+    (left: Array<Object>, leftAccessor: IAccessor<Object, any>,
+     right: Array<Object>, rightAccessor: IAccessor<Object, any>): Array<Object> => {
+        var idx: _.Dictionary = _.indexBy(right, rightAccessor);
+        return _.filter(left, (datum: Object) => !_.has(idx, leftAccessor(datum)));
+    };
+
+export var HashLeftSemiJoin: IJoin<Object, Object, any, Object> =
+    (left: Array<Object>, leftAccessor: IAccessor<Object, any>,
+     right: Array<Object>, rightAccessor: IAccessor<Object, any>): Array<Object> => {
+        var idx: _.Dictionary = _.indexBy(right, rightAccessor);
+        return _.filter(left, (datum: Object) => _.has(idx, leftAccessor(datum)));
+    };
