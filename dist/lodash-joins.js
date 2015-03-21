5973f65785ebd82139745ebab4a0760522f8b6b3
Skitch <mrt6467@alum.uncw.edu>
2015-03-20 20:47:27 -0400

    Update index file to use fresh lodash context

--- dist/lodash-joins.js
+++ dist/lodash-joins.js
@@ -54,7 +54,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var _ = __webpack_require__(1);
+	var _ = __webpack_require__(1).runInContext();
 	
 	var joinWrapper = __webpack_require__(2);
 	
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var _ = require('lodash');
+var _ = require('lodash').runInContext();
 
 var joinWrapper = require('./lib/joinWrapper');
 
